e265449203bc5b9e423a2483c6078e1ee1ebdb6f
Alexandre MAUROUARD <alexandre.maurouard@wanadoo.fr>
2015-03-22 00:00:47 +0100

    Commencement Partie 2

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -5,12 +5,23 @@ class UserController extends ControllerBase
 
     public function projectAction($id)
     {
+<<<<<<< HEAD
         $project = Projet::findFirst($id);
         $messages = $project->getAllMessages();
 
         if (!is_null($project)) {
             $this->view->setVar('project', $project);
             $this->view->setVar('nbMessages', count($messages));
+=======
+        $project = Projet::findFirst($idclient);
+        $client = User::findFirst($id);
+
+
+        if (!is_null($project)) {
+            $this->view->setVar('project', $project);
+
+
+>>>>>>> Commencement Partie 2
         } else {
             // show 404 error
         }
