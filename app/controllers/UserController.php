4d71d6527196b11ad530842e7b4cb0bfbcec6f1e
Alexandre MAUROUARD <alexandre.maurouard@wanadoo.fr>
2015-03-22 00:07:55 +0100

    Résolution des conflits

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -5,25 +5,22 @@ class UserController extends ControllerBase
 
     public function projectAction($id)
     {
-<<<<<<< HEAD
+
         $project = Projet::findFirst($id);
         $messages = $project->getAllMessages();
 
         if (!is_null($project)) {
             $this->view->setVar('project', $project);
             $this->view->setVar('nbMessages', count($messages));
-=======
-        $project = Projet::findFirst($idclient);
-        $client = User::findFirst($id);
 
 
-        if (!is_null($project)) {
-            $this->view->setVar('project', $project);
+            if (!is_null($project)) {
+                $this->view->setVar('project', $project);
 
 
->>>>>>> Commencement Partie 2
-        } else {
-            // show 404 error
+            } else {
+                // show 404 error
+            }
         }
     }
 
